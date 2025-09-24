efb1768a72419c1d906f118c203aaf8b6683215f
mk1316 <kfesaha15@gmail.com>
2025-09-23 20:54:13 -0500

    Adding first 42 audio files

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,7 +4,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Enable package exports for web support with import.meta
-// config.resolver.unstable_enablePackageExports = true;
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.resolver.unstable_enablePackageExports = true;
 
 module.exports = config;
